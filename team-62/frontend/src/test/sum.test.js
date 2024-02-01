ac63d0e011ec54215f3ff4f1ec36ca296872ff7a
PascalBBcn <pascalbbcn@gmail.com>
2024-02-01 16:00:42 +0100

    fixed testing frontend

---
+++ team-62/frontend/src/test/sum.test.js
@@ -0,0 +1,11 @@
+const sum = require('../components/sum.js');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+const login = require('../components/Login.js');
+
+test('adds 1 + 2 to equal 3', () => {
+  //expect(sum(1, 2)).toBe(3);
+});
